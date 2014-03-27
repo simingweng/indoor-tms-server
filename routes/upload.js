24a5eeab97074e1b49227c508717700462457a11
SiMing Weng <siming.weng@gmail.com>
2014-03-27 13:18:06 +0800

    add floor routes for adding, deleting, and modifying a floor of a building add upload route to accept image floor plan move floor and gcp schema into separate models

---
+++ routes/upload.js
@@ -0,0 +1,15 @@
+/**
+ * Created by simingweng on 27/3/14.
+ */
+var formidable = require('formidable');
+
+exports.upload = function (req, res) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        if (err) {
+            res.send(500, err);
+        } else {
+            res.json(files);
+        }
+    });
+};
